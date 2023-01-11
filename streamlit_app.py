a7a25616151082910f12635b7f0764215c87bffe
Kristian Carroll Labs <122119160+kcarrollLabs@users.noreply.github.com>
2023-01-11 14:08:43 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,8 +58,19 @@ if streamlit.button('Get Fruit Load List'):
   streamlit.dataframe(my_data_rows)
 
 #adding a spot where people can add to the list of fruits
-streamlit.write('[What fruit would you like to add?')
-fruit_added = streamlit.text_input('What fruit would you like to add?','Jackfruit')
-streamlit.write('Thanks for adding', fruit_added)
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("Insert into fruit_load_list values (new_fruit)")
+    return "Thanks for adding" + new_fruit
 
-#my_cur.execute("Insert into fruit_load_list values ('from streamlit')")
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
+if streamlit.button('Add a Fruit to the List')
+   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+   back_from_function = insert_row_snowflake(add_my_fruit)
+   streamlit.text(back_from_function)
+  
+# streamlit.write('[What fruit would you like to add?')
+# add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
+# streamlit.write('Thanks for adding', fruit_added)
+
+# my_cur.execute("Insert into fruit_load_list values ('from streamlit')")
